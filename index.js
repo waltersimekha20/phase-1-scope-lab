2fea81121fe0234e1d85fcedfb6ddee7a8ea36ba
waltersimekha <waltersimekha@gmail.com>
2023-03-21 08:25:48 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+//Fix the scope
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+
+//best customer
+
+function setBestCustomer(){
+     bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+    bestCustomer ='maybe bob'
+}
+
+
+const leastFavoriteCustomer = 'bob';
+function changeLeastFavoriteCustomer(){  
+    leastFavoriteCustomer ='jane'
+}
